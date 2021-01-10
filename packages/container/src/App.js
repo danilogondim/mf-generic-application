f0a19c70b3ff95c6e0d5f9d973b7540b5e1c2d49
Danilo Gondim <daniloencarnacao@gmail.com>
2021-01-09 21:57:43 -0500

    Add access control to the dashboard microfrontend

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,7 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom'; // after adding dashboard, we are importing Router instead of BrowserRouter. The only reason is that we need access to the browser history in the same component (App.js) where the BrowserRouter was implicitly creating a history object. To access this object in this very specific level, we need to use the Router component and to keep this refactor working as before we then pass a copy of the browser history as prop. If we had to access the history in a children component, we could had used the BrowserHistory (in fact we were doing this before such change)
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import { createBrowserHistory } from 'history';
 
 import Progress from './components/Progress';
 import Header from './components/Header';
@@ -13,10 +14,21 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
+// create the browser history to pass to the Router component (BrowserHistory was implicitly doing this for us before, but now we need access to this object to controle dashboard access)
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard');
+    }
+    // we can't use an else statement to redirect not logged users to homepage because if we do this, every user that gets to our application will be automatically redirected to the homepage (and maybe he/she is just trying to see the pricing page and not the dashboard as we want to control the access)
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header
@@ -28,12 +40,15 @@ export default () => {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
-              <Route path="/dashboard" component={DashboardLazy} />
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to='/' /> /* if the user tries to go to the dashboard route, but it is not signed in, then we will mount the redirect component (that does exactly what the name says, redirects the user to the specified path (homepage) */}
+                <DashboardLazy />
+              </Route>
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
